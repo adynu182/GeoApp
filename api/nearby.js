babfee6f00d6563efeda90e8ad218e513790e438
ADY NU <adynu182@gmail.com>
2026-04-21 08:04:39 +0700

    Update nearby.js

--- api/nearby.js
+++ api/nearby.js
@@ -26,7 +26,9 @@ export default async function handler(req, res) {
 
     res.status(200).json(result.rows);
   } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: "error" });
+    console.error("ERROR DETAIL:", err.message);
+    return res.status(500).json({
+    error: err.message
+    });
   }
 }

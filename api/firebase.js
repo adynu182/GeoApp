41db65caca3d98b50850f1ea4afab2786ca1fc35
ADY NU <adynu182@gmail.com>
2026-05-24 12:39:42 +0700

    Create firebase.js

---
+++ api/firebase.js
@@ -0,0 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyB0pZijSo-v2w-VCmrORb9CowMCBQ6-ws0",
+    authDomain: "geo-app-fd860.firebaseapp.com",
+    projectId: "geo-app-fd860",
+    storageBucket: "geo-app-fd860.firebasestorage.app",
+    messagingSenderId: "469722777353",
+    appId: "1:469722777353:web:e83f3e2871be2caf0335d8"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
